0bdfd26472f4fedba663f116afe2f843ca9b2f28
MicaelLoio <131375089+MicaelLoio@users.noreply.github.com>
2024-02-08 00:00:45 +0000

    Flocking Part 3
    add a goal position to the flocking simulation. check the natural speed of the floating fishes and changed scripts

--- flockingUnity/Assets/FlockManager.cs
+++ flockingUnity/Assets/FlockManager.cs
@@ -9,6 +9,7 @@ public class FlockManager : MonoBehaviour
     public int numFish = 20;
     public GameObject[] allFish;
     public Vector3 swimLimits = new Vector3(5, 5, 5);
+    public Vector3 goalPos = Vector3.zero;
 
 
     [Header("Fish Settings")]
@@ -35,11 +36,18 @@ public class FlockManager : MonoBehaviour
 
 			  }
         FM = this;
+        goalPos = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if(Random.Range(0,100) <10)
+        {
+            goalPos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+																		 Random.Range(-swimLimits.y, swimLimits.y),
+																		  Random.Range(-swimLimits.z, swimLimits.z));
+		}
+
+	}
 }
